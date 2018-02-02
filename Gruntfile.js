851f67c7450609c142b2ed14cf24ab6db092a93f
nmastracci <mastracci.natalie@gmail.com>
2018-02-02 10:35:36 -0500

    adding routes and package locks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,45 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg : grunt.file.readJSON('package.json'), 
-		concat : {
-			dist: {
-				src: [
-				'js/modules/*.js',
-				'js/main.js'
-				],
-				dest: 'prod/production.js'
-			}
-		},
+  // 1. all config stuff goes here
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'), // reads the packages etc inside that file
+      // uglify: {
+      //   build: {
+      //     src: 'public/js/main.js',
+      //     dest: 'public/js/main.min.js'
+      //   }
+      // },
 
-		uglify : {
-			build: {
-				src: 'prod/production.js',
-				dest: 'prod/production.min.js'
-			}
-		},
+      watch: {
+        // scripts: {
+        //   files: ['public/js/main.js'],
+        //   tasks: ['uglify'],
+        //   options: {
+        //     spawn: false
+        //   }
+        // },
 
-		sass : {
-			build: {
-				loadPath: '.',
-				src: ['sass/modules/*.scss',
-					'sass/main.scss'],
-				dest: 'prod/production.css'
-			}
-		},
+        sass: {
+          files: ['sass/main.scss'],
+          tasks: ['sass']
+        }
+      },
 
-		watch : {
-			scripts: {
-				files: ['js/main.js', 'js/modules/*.js', 'sass/modules/*.scss', 'sass/main.scss'],
-				tasks: ['concat', 'uglify', 'sass'],
-				options: {
-					spawn: false
-				}
-			}
-		}
-	});
+      sass: {
+        dist: {
+          options: {
+            style: 'compressed'
+          },
+          files: {
+            'public/css/main.css':'sass/main.scss'
+          }
+        }
+      }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  // grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
-
-	grunt.registerTask('default', ['concat', 'uglify']);
-	grunt.registerTask('watchFiles', ['watch']);
+  grunt.registerTask('default', ['sass']); //'uglify', 
+  grunt.registerTask('watchFiles', ['watch']);
 };
--- app.js
+++ app.js
@@ -1,15 +1,29 @@
+
 const express = require('express'); //include the express object
-
 const app = express(); //instaniate new express object
+const io = require('socket.io')();//the round brackets instantiate it
 
-app.get('/', (req, res) =>{
-	res.sendFile(__dirname + '/index.html');
-});//tells which file to serve. this will route the URL
-app.get('/contact', (req, res) =>{
-	res.sendFile(__dirname + '/contact.html');
-});
+//serve up static js and css  files
+app.use(express.static('public'));
+
+//add routes
+app.use(require('./routes/index'));
+app.use(require('./routes/contact'));
 
 
-app.listen(3000, ()=> {
+// app.get('/', (req, res) =>{
+// 	res.sendFile(__dirname + '/index.html');
+// });//tells which file to serve. this will route the URL
+// app.get('/contact', (req, res) =>{
+// 	res.sendFile(__dirname + '/contact.html');
+// });
+
+
+const server = app.listen(3000, () => {//making this a const gives socket a thing to listen to
 	console.log('listening on Port 3000');
 });//where are we serving it.. this consoles out to the terminal and not the browser
+
+io.attach(server); //this attaches socket to the server and lets it know that it has to listen for messages back and forth
+io.on('connection', socket => { 
+	console.log('a user has connected');
+});
