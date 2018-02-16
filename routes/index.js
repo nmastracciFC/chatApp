b69f5b840aab88edb66e767741f250fa0b55e97e
nmastracci <mastracci.natalie@gmail.com>
2018-02-15 23:40:09 -0500

    styling complete

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const path = require('path');
 router.get('/', (req, res) =>{
 	res.sendFile(path.resolve(__dirname, '../views/index.html'));
 });//tells which file to serve. this will route the URL
-
+router.get('/chat', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../views/chat.html'));
+});
 
 module.exports = router;
