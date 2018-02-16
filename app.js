8cc5eda7098b1fcad9cd9178e4cf70f0af6544f8
nmastracci <mastracci.natalie@gmail.com>
2018-02-16 02:00:03 -0500

    changing features

--- app.js
+++ app.js
@@ -1,36 +1,26 @@
 
-const express = require('express'); //include the express object
-const app = express(); //instaniate new express object
-const io = require('socket.io')();//the round brackets instantiate it
+const express = require('express'); 
+const app = express(); 
+const io = require('socket.io')();
 
-//serve up static js and css  files
 app.use(express.static('public'));
 
 //add routes
 app.use(require('./routes/index'));
 app.use(require('./routes/contact'));
 
-
-// app.get('/', (req, res) =>{
-// 	res.sendFile(__dirname + '/index.html');
-// });//tells which file to serve. this will route the URL
-// app.get('/contact', (req, res) =>{
-// 	res.sendFile(__dirname + '/contact.html');
-// });
-
-
-const server = app.listen(3000, () => {//making this a const gives socket a thing to listen to
+const server = app.listen(3000, () => {
 	console.log('listening on Port 3000');
-});//where are we serving it.. this consoles out to the terminal and not the browser
+});
 
-io.attach(server); //this attaches socket to the server and lets it know that it has to listen for messages back and forth
-
-users = [];
+io.attach(server); 
 io.on('connection', socket => { 
 	console.log('a user has connected');
 	io.emit('chat message', {for: 'everyone', message: `${socket.id} is here`});
+
 //this is the function that allows chat messages to be shot back and forth along the socket.io string
 socket.on('chat message', msg => {
+	console.log('message: ', msg);
 	io.emit('chat message', { for: 'everyone', message: msg});
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) =>{
-	res.sendFile(path.resolve(__dirname, '../views/index.html'));
+	res.sendFile(path.resolve(__dirname, '../views/chat.html'));
 });//tells which file to serve. this will route the URL
 router.get('/chat', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../views/chat.html'));
+	res.sendFile(path.resolve(__dirname, '../views/index.html'));
 });
 
 module.exports = router;
